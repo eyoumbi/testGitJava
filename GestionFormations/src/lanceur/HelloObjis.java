330b262ddc9c69716c2977d3ea884794e23994cd
HUIOS <services.huios@gmail.com>
2018-05-24 23:32:37 +0200

    projet java exemple

---
+++ GestionFormations/src/lanceur/HelloObjis.java
@@ -0,0 +1,24 @@
+package lanceur;
+
+/**
+ * Classe de démarrage de l'application
+ * @author Douglas MBIANDOU
+ * @version 0.1
+ */
+
+public class HelloObjis {
+
+	/**
+	 * La méthode main est statique : elle est chargée en mémoire
+	 * au démarrage de l'application et peut donc être utilisée avant 
+	 * la création de tout objet.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+        System.out.println("Bonjour Objis, spécialiste formation Java");
+	}
+
+}
